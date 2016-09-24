d2baa6d4c05db7019e7dcd020c4062c8f53ffd10
Thiago Senna <trsenna@gmail.com>
2016-09-23 21:25:49 -0300

    initial version

---
+++ includes/functions.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @package    My\Site\Functionality
+ * @author     Thiago Senna <thiago@thremes.com.br>
+ * @copyright  Copyright (c) 2016, Thiago Senna
+ * @license    http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ */
+
+/* Disallow Site Changes
+------------------------------------------------------- */
+add_action( 'plugins_loaded', function () {
+    defined( 'DISALLOW_FILE_EDIT' ) or define( 'DISALLOW_FILE_EDIT', TRUE );
+    defined( 'DISALLOW_FILE_MODS' ) or define( 'DISALLOW_FILE_MODS', TRUE );
+    defined( 'AUTOMATIC_UPDATER_DISABLED' ) or define( 'AUTOMATIC_UPDATER_DISABLED', TRUE );
+} );
+
+/* Disallow Pings and Comments
+------------------------------------------------------- */
+add_action( 'plugins_loaded', function () {
+    add_filter( 'comments_open', '__return_false' );
+    add_filter( 'pings_open', '__return_false' );
+} );
+
+/* Makes Jetpack Developer Friendly by default
+------------------------------------------------------- */
+add_action( 'plugins_loaded', function () {
+    defined( 'JETPACK_DEV_DEBUG' ) or define( 'JETPACK_DEV_DEBUG', TRUE );
+    add_filter( 'jetpack_get_default_modules', '__return_empty_array', 99 );
+} );
---
+++ site-functionality.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name: My/Site/Functionality
+ * Description: The My/Site/Functionality plugin.
+ *
+ * Author: Thiago Senna
+ * Author URI: http://thremes.com.br
+ *
+ * @package   My\Site\Functionality
+ * @author    Thiago Senna <thiago@thremes.com.br>
+ * @copyright Copyright (c) 2016, Thiago Senna
+ * @license   http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ */
+require_once( dirname( __FILE__ ) . '/includes/third/class-tgm-plugin-activation.php' );
+require_once( dirname( __FILE__ ) . '/includes/functions.php' );
